65998aaebbc5e3cea400202c2d82c1d654d196b1
minsuk3238 <minsuk3238@users.noreply.github.com>
2026-07-25 11:58:53 +0900

    Remove grantOfflineAccess to fix 400 error on Android

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 
 GoogleAuth.initialize({
   clientId: '123154076628-oql4c5l6dr9v4d96o2u8f9ujn792nsq1.apps.googleusercontent.com',
-  scopes: ['profile', 'email', 'https://www.googleapis.com/auth/calendar.events'],
-  grantOfflineAccess: true,
+  scopes: ['profile', 'email', 'https://www.googleapis.com/auth/calendar.events']
 });
 
 createRoot(document.getElementById('root')).render(
